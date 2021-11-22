a50ace8aea01ba45a38a43565d404bee2c752cbc
Gynna Paola Mogollon Zabala <gynnapmz@gmail.com>
2021-11-22 00:48:22 -0500

    nuevo

---
+++ Back/restauranteeApi/Controllers/EmpleadosController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using restauranteeApi.Models;
+
+namespace restauranteeApi.Controllers
+{
+    [Route("api/empleados[controller]")]
+    [ApiController]
+    public class EmpleadosController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _env;
+        public EmpleadosController(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            _configuration = configuration;
+            _env = env;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+
+            string query = @"
+                         select id, nombre, cargo, imagen
+                         from 
+                         empleados
+        ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("TestAppCon");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+                        delete from empleados 
+                        where id=@EmpleadosId;
+                        
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("TestAppCon");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myCommand.Parameters.AddWithValue("@EmpleadosId", id);
+
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            return new JsonResult("Deleted Successfully");
+        }
+
+        //ACTUALIZACIÓN
+
+
+        [HttpPut]
+        public JsonResult Put(Empleados emp)
+        {
+            string query = @"
+                        update empleado set 
+                        nombre =@EmpleadosNombre,
+                        cargo =@EmpleadosCargo,
+                        imagen =@EmpleadosImagen,
+                        where id =@EmpleadosId;
+                        
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("TestAppCon");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myCommand.Parameters.AddWithValue("@EmpleadosId", emp.id);
+                    myCommand.Parameters.AddWithValue("@EmpleadosNombre", emp.nombre);
+                    myCommand.Parameters.AddWithValue("@EmpleadosCargo", emp.cargo);
+                    myCommand.Parameters.AddWithValue("@EmpleadosImagen", emp.imagen);
+
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            return new JsonResult("Updated Successfully");
+        }
+        //CREACIÓN
+
+        [HttpPost]
+        public JsonResult Post(Models.Empleados emp)
+        {
+            string query = @"
+                        insert into empleados 
+                        (nombre,cargo, imagen) 
+                        values
+                         (@EmpleadosNombre,@EmpleadosCargo, @EmpleadosImagen) ;
+                        
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("TestAppCon");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myCommand.Parameters.AddWithValue("@EmpleadosNombre", emp.nombre);
+                    myCommand.Parameters.AddWithValue("@Empleadoscargo", emp.cargo);
+                    myCommand.Parameters.AddWithValue("@EmpleadosImagen", emp.imagen);
+
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            return new JsonResult("Added Successfully");
+        }
+    }
+}
+
---
+++ Back/restauranteeApi/Models/Empleados.cs
@@ -0,0 +1,10 @@
+﻿namespace restauranteeApi.Models
+{
+    public class Empleados
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string cargo { get; set; }
+        public string imagen { get; set; }
+    }
+}
