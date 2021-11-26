33f39f44cea0487966029f224e15cfb03f2dff8b
CarlosMCL <cmconrado@unicesar.edu.co>
2021-11-25 20:14:13 -0500

    as

---
+++ IntentoOne/WebApplication1/Controllers/ClientesController.cs
@@ -0,0 +1,191 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _env;
+        public  ClientesController(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            _configuration = configuration;
+            _env = env;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                        select id,nombre,descripcion,nombreusuario,password
+                        from Cliente
+                       
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("TestAppCon");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+        //CONSULTA
+        [HttpGet("{id}")]
+
+        public JsonResult Get(int id)
+        {
+            string query = @"
+                        select id,nombre,descripcion,nombreusuario,password
+                        from Cliente
+                        where id=@ClienteId;
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("TestAppCon");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myCommand.Parameters.AddWithValue("@ClienteId", id);
+
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+        //ELIMINACION
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+                        delete from Cliente 
+                        where id=@ClienteId;
+                        
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("TestAppCon");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myCommand.Parameters.AddWithValue("ClienteId", id);
+
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            return new JsonResult("Deleted Successfully");
+        }
+
+        //ACTUALIZACIÓN
+
+
+        [HttpPut("{id}")]
+        public JsonResult Put(Cliente cli, int id)
+        {
+            string query = @"
+                        update Cliente set 
+                        nombre =@ClienteNombre,
+                        descripcion =@ClienteDescripcion,
+                        nombreusuario =@ClienteNombreUsuario,
+                        password =@ClientePassword
+                        where id =@ClienteId;
+                        
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("TestAppCon");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myCommand.Parameters.AddWithValue("@ClienteId", cli.id);
+                    myCommand.Parameters.AddWithValue("@ClienteNombre", cli.nombre);
+                    myCommand.Parameters.AddWithValue("@ClienteDescripcion", cli.descripcion);
+                    myCommand.Parameters.AddWithValue("@ClienteNombreUsuario", cli.nombreusuario);
+                    myCommand.Parameters.AddWithValue("@ClientePassword", cli.password);
+
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            return new JsonResult("Updated Successfully");
+        }
+        //CREACIÓN
+
+        [HttpPost]
+        public JsonResult Post(Models.Cliente cli)
+        {
+            string query = @"
+                        insert into Cliente 
+                        (nombre,descripcion,nombreusuario,password) 
+                        values
+                         (@ClienteNombre,@ClienteDescripcion,@ClienteNombreUsuario,@ClientePassword) ;
+                        
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("TestAppCon");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myCommand.Parameters.AddWithValue("@ClienteNombre", cli.nombre);
+                    myCommand.Parameters.AddWithValue("@ClienteDescripcion", cli.descripcion);
+                    myCommand.Parameters.AddWithValue("@ClienteNombreUsuario", cli.nombreusuario);
+                    myCommand.Parameters.AddWithValue("@ClientePassword", cli.password);
+
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            return new JsonResult("Added Successfully");
+        }
+    }
+}
