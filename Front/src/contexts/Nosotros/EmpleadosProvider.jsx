8905585103add153cd3c70feede1b85501e44f6b
manuel231509 <manuel231509@gmail.com>
2021-11-27 00:06:23 -0500

    Cambios

--- Front/src/contexts/Nosotros/EmpleadosProvider.jsx
+++ Front/src/contexts/Nosotros/EmpleadosProvider.jsx
@@ -1,10 +1,19 @@
 import React, { useState } from "react";
 import EmpleadosContext from "./EmpleadosContext";
 // import DatosTrabajadores from "../../consts json/Nosotros/DatosTrabajadores.json";
-import { getEmpleados } from "../../services/EmpleadoService/Empleado";
+// import { getEmpleados } from "../../services/EmpleadoService/Empleado";
 
 const EmpleadosProvider = ({ children }) => {
-	const [modalTrabajadores, setModalTrabajadores] = useState(getEmpleados());
+	const getEmpleados = () => {
+		fetch("http://localhost:15937/api/empleado")
+			.then((response) => response.json())
+			.then((data) => {
+				console.log("data --- ", data);
+			});
+	};
+
+	getEmpleados();
+	const [modalTrabajadores, setModalTrabajadores] = useState();
 
 	const handleChangeModalTrabajadores = (valor) => {
 		setModalTrabajadores(valor);
--- Front/src/services/EmpleadoService/Empleado.js
+++ Front/src/services/EmpleadoService/Empleado.js
@@ -1,11 +1,19 @@
-const getEmpleados = () => {
-	let empleados = [];
-	fetch("http://localhost:15937/api/empleado")
-		.then((response) => response.json())
-		.then((data) => {
-			empleados = data;
-		});
-	return empleados;
+import React from "react";
+
+const Empleado = () => {
+	const getEmpleados = () => {
+		let empleados = [];
+		fetch("http://localhost:15937/api/empleado")
+			.then((response) => response.json())
+			.then((data) => {
+				empleados = data;
+			});
+		return empleados;
+	};
+
+	return <></>;
 };
+
+export default Empleado;
 
 export { getEmpleados };
